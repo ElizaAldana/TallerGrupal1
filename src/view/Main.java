33ae19bca0fd96b98835f7060f1092a5306b89a6
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-05 12:13:25 -0500

    clases y libreria
    mas tarde pongo el resto

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,13 @@
-package spaceApp;
+package view;
 
-public class main {
+import controller.Controller;
+
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	Controller control;
 }
