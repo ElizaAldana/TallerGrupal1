82d7e46292182cb1f1e832f12eadcfdc943ea33a
jaimedayo <53444398+jaimedayo@users.noreply.github.com>
2020-11-05 14:39:12 -0500

    constructor clase person
    hice llena la clase person y puse los metodos geters

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,38 @@
 package model;
 
 public class Person {
+	
+private String name;
+private String lastn;
+private String email;
+private String nation;
+private int phone;
 
+public Person(String name,String lastn,String email,String nation, int phone) {
+	this.name=name;
+	this.lastn=lastn;
+	this.email=email;
+	this.nation=nation;
+	this.phone=phone;
+}
+
+public String getName() {
+	return this.name;
+}
+
+public String getLastn() {
+	return this.lastn;
+}
+
+public String getEmail() {
+	return this.email;
+}
+
+public String getNation() {
+	return this.nation;
+}
+
+public int getPhone() {
+	return this.phone;
+}
 }
