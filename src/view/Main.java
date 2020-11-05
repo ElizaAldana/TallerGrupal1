b73854872876cc1c8b41e5faf5f6878e8037842a
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-05 12:46:28 -0500

    corregido el paquete

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,13 +1,21 @@
 package view;
 
-import controller.Controller;
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
-	Controller control;
+	public void settings() {
+		size(500,500);
+	}
+	
+	public void setup() {
+	}
+	
+	public void draw() {
+		background(0);
+}
 }
