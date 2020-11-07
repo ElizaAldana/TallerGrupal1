62754c96eba009ac102c2ae0f4f5a3668c6cfb81
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-07 17:23:39 -0500

    corrección pero sigue mal

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -2,21 +2,17 @@ package model;
 
 public class Contact extends Person implements Comparable <Contact>{
 
+
+
 	public Contact(String name, String lastn, String email, String nation, int phone) {
 		super(name, lastn, email, nation, phone);
 		// TODO Auto-generated constructor stub
 	}
 
 	
-	public int compareTo(Contact o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 	
-	//public int compareTo(Contact c) {
-		// TODO Auto-generated method stub
-		//return this.name - c.getName();
+	public String compareTo(Contact c) {
+		return this.name - c.getName();
 	}
 
 //}
