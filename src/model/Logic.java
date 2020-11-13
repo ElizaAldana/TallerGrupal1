176ccf904f9ce9e91d43c44a0806cc7fe9324d44
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-13 11:45:12 -0500

    sort :) aun me falta

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,6 +1,8 @@
 package model;
 
 import processing.core.PApplet;
+
+import java.util.Collections;
 import java.util.LinkedList;
 
 
@@ -11,9 +13,17 @@ public class Logic {
 	private PApplet app ;
 	public Contact contacto;
 	private LinkedList<Contact> contacts;
-
+	private CompareEmailContact email;
+	private CompareLastNContact lastn;
+	private CompareNationContact nation;
+	private ComparePhoneContact phone;
+	
 	public Logic(PApplet app) {
 		this.app=app;
+		this.email= new CompareEmailContact();
+		this.lastn= new CompareLastNContact();
+		this.nation= new CompareNationContact();
+		this.phone= new ComparePhoneContact();
 		
 		contacts = new LinkedList<Contact>();
 		
@@ -33,11 +43,38 @@ public class Logic {
 		 contacts.add(new Contact(name, lastn, email, nation, phone,app));
 
 	}
+
+	public void sortList(char s) {
+		switch (s) { 
+		case 'c':
+			Collections.sort(contacts);
+			break;
+		case 'l':
+			Collections.sort(contacts,lastn);
+			break;
+		case 'e':
+			Collections.sort(contacts,email);
+			break;
+		case 'n':
+			Collections.sort(contacts,nation);
+		break;
+		case 'p':
+			Collections.sort(contacts, phone);
+		break;
+		default:
+			break;
+		}
+	}
 	
 	public LinkedList<Contact> getList() {
 		return contacts;
 
+	}
+
+	
+	public void setContact(LinkedList<Contact> contacts) {
+		this.contacts = contacts;
+	
+			
 }
-			
-			
 }
