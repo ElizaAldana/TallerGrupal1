5c850b3832207005167683f786e8aa9690547cbc
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-13 15:29:11 -0500

    arreglos en la clase card

--- src/model/Card.java
+++ src/model/Card.java
@@ -2,4 +2,12 @@ package model;
 
 public class Card {
 
+	private String titular, date;
+	private int number, cvv;
+	
+	public Card (String titular, String date, int number, int cvv) {
+		super();
+		
+		
+	}
 }
